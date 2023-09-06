874cadd40dae55f8eb03b46ed791516f2d0a729b
David Plaskett <mrtinguy@gmail.com>
2023-09-06 13:51:40 -0600

    updated the docker files and all set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and Go for the win!
+          Edit <code>src/App.js</code> ^_^
         </p>
         <a
           className="App-link"
